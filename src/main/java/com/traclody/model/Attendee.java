477e7fb1f78dbdf55cc53b40b44980b3364600bb
Clody01 <vroclaude@gmail.com>
2016-01-31 18:53:32 +0100

    Adding Validation

--- src/main/java/com/traclody/model/Attendee.java
+++ src/main/java/com/traclody/model/Attendee.java
@@ -1,10 +1,18 @@
 package com.traclody.model;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class Attendee {
 
-	
+	@Size(min=8, max=30)
 	private String nom;
+	
+	@NotEmpty @Email
 	private String email;
+	
 	public String getNom() {
 		return nom;
 	}
