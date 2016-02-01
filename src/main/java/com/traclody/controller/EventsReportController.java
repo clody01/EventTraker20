d7a9c19546e721a9a6db0096a609bb104d963c13
Clody01 <vroclaude@gmail.com>
2016-02-01 12:01:13 +0100

    Angular Client

--- src/main/java/com/traclody/controller/EventsReportController.java
+++ src/main/java/com/traclody/controller/EventsReportController.java
@@ -1,8 +1,9 @@
 package com.traclody.controller;
 
-import java.util.LinkedList; 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
- 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,20 +13,21 @@ import com.traclody.model.Event;
 public class EventsReportController {
 	
 	@RequestMapping(value="/events")
-	public LinkedList<Event> getEvent(){		 
-		LinkedList<Event> events = new LinkedList<>();
+	public List<Event> getEvent(){	
+		//les arraylistes peuvent etre transmisent avec REST
+		List<Event> events = new ArrayList<>();
 		Event event1 = new Event();
 		event1.setNom("Tra");
-		event1.setPrenom("Tra");
+		event1.setPrenom("clody");
 		Event event2 = new Event();
 		event2.setNom("Traore");
-		event2.setPrenom("Traore");
+		event2.setPrenom("adama");
 		Event event3 = new Event();
 		event3.setNom("Tranan");
-		event3.setPrenom("Tranan");
+		event3.setPrenom("pauline");
 		Event event4 = new Event();
 		event4.setNom("Tralou");
-		event4.setPrenom("Tralou");
+		event4.setPrenom("Jacqueline");
 		events.add(event1);
 		events.add(event2);
 		events.add(event3);
